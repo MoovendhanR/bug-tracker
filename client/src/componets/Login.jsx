9ee40c03e06348aa7434c30480b6cc0148abae2d
MoovendhanR <moovendhanr1011@gmail.com>
2023-03-31 01:31:30 +0530

    login part succesfully done with a token

--- client/src/componets/Login.jsx
+++ client/src/componets/Login.jsx
@@ -1,10 +1,91 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {
+    Flex,
+    Box,
+    FormControl,
+    FormLabel,
+    Input,
+    Checkbox,
+    Stack,
+    Link,
+    Button,
+    Heading,
+    Text,
+    useColorModeValue,
+  } from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate=useNavigate();
+ 
+    const handleSubmit=()=>{
+          const payload = {
+            email,
+            password,
+          }
+          fetch("http://localhost:5000/users/login",{
+            method:"POST",
+            body:JSON.stringify(payload),
+            headers:{
+               "Content-type": "application/json"
+            }
+         })
+         .then(res=>res.json())
+         .then(res=>console.log(res))
+         .then(()=>alert("Login succesfull"))
+         .then(()=>navigate("/dashboard"))
+         .catch(err=>console.log(err))
+    }
+
   return (
-    <div>
-       user login
-    </div>
+    <Flex
+    minH={'100vh'}
+    align={'center'}
+    justify={'center'}
+    bg={useColorModeValue('gray.50', 'gray.800')}>
+    <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+      <Stack align={'center'}>
+        <Heading fontSize={'4xl'}>Sign in to your account</Heading>
+      </Stack>
+      <Box
+        rounded={'lg'}
+        bg={useColorModeValue('white', 'gray.700')}
+        boxShadow={'lg'}
+        p={8}>
+        <Stack spacing={4}>
+          <FormControl id="email">
+            <FormLabel>Email address</FormLabel>
+            <Input type="email" value={email} onChange={(e)=>setEmail(e.target.value)} />
+          </FormControl>
+          <FormControl id="password">
+            <FormLabel>Password</FormLabel>
+            <Input type="password" value={password} onChange={(e)=>setPassword(e.target.value)}/>
+          </FormControl>
+          <Stack spacing={10}>
+            <Stack
+              direction={{ base: 'column', sm: 'row' }}
+              align={'start'}
+              justify={'space-between'}>
+              <Checkbox>Remember me</Checkbox>
+              <Link color={'blue.400'}>Forgot password?</Link>
+            </Stack>
+            <Button
+              bg={'blue.400'}
+              color={'white'}
+              _hover={{
+                bg: 'blue.500',
+              }}
+              onClick={handleSubmit}
+              >
+              Sign in
+            </Button>
+          </Stack>
+        </Stack>
+      </Box>
+    </Stack>
+  </Flex>
   )
 }
 
